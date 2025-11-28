2777bb8d92bf69e7494e298cc761c6aac06a8dd8
BonnieSimmer <noura19918@gmail.com>
2025-11-28 09:24:26 +0200

    Implemented the movie recommendation method for a single user

--- src/main/java/com/mycompany/movierecommendationsystem/logic/MovieRecommender.java
+++ src/main/java/com/mycompany/movierecommendationsystem/logic/MovieRecommender.java
@@ -7,6 +7,19 @@ import java.util.List;
 
 public class MovieRecommender {
     public List<Movie> recommendMoviesForUser(User user, List<Movie> movies) {
-        return null;
+        List<Movie> favouriteMovies = user.getLikedMovieIds().stream()
+                .map(id -> movies.stream()
+                        .filter(movie -> movie.getId().equals(id))
+                        .findFirst()
+                        .orElse(null))
+                .toList();
+        List<String> favGenres = favouriteMovies.stream()
+                .map(Movie::getGenres)
+                .flatMap(List::stream)
+                .toList();
+
+        return movies.stream()
+                .filter(movie -> movie.getGenres().stream().anyMatch(favGenres::contains))
+                .toList();
     }
 }
