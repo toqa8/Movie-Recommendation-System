ab202f972f5e5021fc6cdbe4e94164053770ef42
BonnieSimmer <noura19918@gmail.com>
2025-12-18 17:17:36 +0200

    Optimized MovieRecommender as the multiple streams made it overly complicated.

--- src/main/java/com/mycompany/movierecommendationsystem/logic/MovieRecommender.java
+++ src/main/java/com/mycompany/movierecommendationsystem/logic/MovieRecommender.java
@@ -3,27 +3,28 @@ package com.mycompany.movierecommendationsystem.logic;
 import com.mycompany.movierecommendationsystem.models.Movie;
 import com.mycompany.movierecommendationsystem.models.User;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class MovieRecommender {
+
     public List<Movie> recommendMoviesForUser(User user, List<Movie> movies) {
         if (user == null || movies == null) {
             throw new IllegalArgumentException("User or movies cannot be null");
         }
 
-        List<Movie> favouriteMovies = user.getLikedMovieIds().stream()
-                .flatMap(id -> movies.stream()
-                        .filter(movie -> movie.getId().equals(id))
-                        .findFirst()
-                        .stream()
-                )
-                .toList();
+        Set<String> likedIds = new HashSet<>(user.getLikedMovieIds());
 
+        Set<Movie> favouriteMovies = new HashSet<>();
+        Set<String> favGenres = new HashSet<>();
 
-        List<String> favGenres = favouriteMovies.stream()
-                .map(Movie::getGenres)
-                .flatMap(List::stream)
-                .toList();
+        for (Movie movie : movies) {
+            if (likedIds.contains(movie.getId())) {
+                favouriteMovies.add(movie);
+                favGenres.addAll(movie.getGenres());
+            }
+        }
 
         return movies.stream()
                 .filter(movie -> !favouriteMovies.contains(movie))
